c22113f10ddde8976991325aeb98d01d87d04e88
jonathansharpe <jonathansharpe54@gmail.com>
2021-08-15 15:46:24 -0700

    small changes

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 const http = require('http');
-
+const fs = require('fs');
 const port = process.env.PORT || 5000;
 
 const server = http.createServer((request, response) => {
-    response.statusCode = 200;
-    response.setHeader('Content-Type', 'text/plain');
-    response.end('Hello World\n\nCool!');
+    response.writeHead(200, {'content-type': 'text/html'});
+    fs.createReadStream('public/pages/index.html').pipe(response);
 });
 
 server.listen(port, (err) => {
-    if (err) {
-        return console.log('Something went wrong ', err);
-    }
-    console.log(`Server Listening on: ${port}`);
+  if (err) {
+    return console.log("Something went wrong");
+  }
+  console.log(`Server listening on ${port}`);
 });
