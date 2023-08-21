96d520576398f72aba2897973f6359439691b4d9
jonathansharpe <jonathansharpe54@gmail.com>
2023-08-21 15:50:18 -0700

    Revert "overhauled home page, need to work on borders and stuff"
    This reverts commit 9a5b653efffcd8d8728e5bb4a3420ba3a1c28aa3.

--- public/scripts/gamebuilder.js
+++ public/scripts/gamebuilder.js
@@ -5,7 +5,6 @@ function importHeadModules() {
 	$("head").append("<link href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC' crossorigin='anonymous'>\n");
 	$("head").append("<script src='https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js' integrity='sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM' crossorigin='anonymous'></script>\n");
 	$("head").append("<link rel='stylesheet' type='text/css' href='styles/stylesheet.css'>\n");
-	$("head").append("<link rel='stylesheet' type='text/css' href='styles/index.css'>\n");
 	$("head").append("");
 	return;
 }
@@ -274,7 +273,7 @@ async function teamsSeenList() {
 }
 function buildNavbar() {
 	let output = 
-		"<nav class='navbar navbar-expand-sm bg-light'>\n" +
+		"<nav class='navbar navbar-expand-lg navbar-light bg-secondary rounded'>\n" +
 		"	<div class='container-fluid'>\n" +
 		"		<button class='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarCollapse' aria-controls='navbarCollapse' aria-expanded='false' aria-label='Toggle navigation'>\n" +
 		"			<span class='navbar-toggler-icon'></span>\n"+
@@ -307,9 +306,9 @@ function buildNavbar() {
 		"						<li><a class='dropdown-item' href='yankeestadium.html'>Yankee Stadium II</a></li>\n" +
 		"					</ul>\n"+
 		"				</li>\n" +
-		// "				<li class='nav-item'>\n" +
-		// "					<a class='nav-link' href='tier-list.html'>Tier List</a>\n" +
-		// "				</li>\n" +
+		"				<li class='nav-item'>\n" +
+		"					<a class='nav-link' href='tier-list.html'>Tier List</a>\n" +
+		"				</li>\n" +
 		"				<li class='nav-item'>\n" +
 		"					<a class='nav-link' href='teams-seen.html'>Teams Seen</a>\n" +
 		"				</li>\n" +
