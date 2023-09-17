d8b709f234adea3561a6514449e6a26da3f3fabc
jonathansharpe <jonathansharpe54@gmail.com>
2023-09-17 16:20:11 -0700

    added connection to mongodb, made another json

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
+require('dotenv').config();
 const http = require('http');
 const fs = require('fs');
+const {MongoClient, ServerApiVersion} = require('mongodb');
+const uri = process.env.MONGODB_URI;
 const port = process.env.PORT || 5000;
 const express = require('express');
+
+// console.log("uri:",uri);
 
 const path = require('path');
 const app = express();
@@ -10,6 +15,26 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use('/styles', express.static(__dirname + 'public/styles'));
 app.use('/scripts', express.static(__dirname + 'public/scripts'));
+
+const client = new MongoClient(uri, {
+	serverApi: {
+		version: ServerApiVersion.v1,
+		strict: true,
+		deprecationErrors: true,
+	}
+});
+
+async function run() {
+	try {
+		await client.connect();
+
+		await client.db("admin").command({ping:1});
+		console.log("Pinged your deployment. You successfully connected to MongoDB.");
+	} finally {
+		await client.close();
+	}
+}
+run().catch(console.dir);
 
 app.get('', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
