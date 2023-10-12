4dbb40019b3f95d6352c43b83bd8fd48b36c2883
jonathansharpe <jonathansharpe54@gmail.com>
2023-10-12 16:14:43 -0700

    can read from mongodb

--- server/handlers/games.js
+++
@@ -1,9 +0,0 @@
-
-async function fetchGames(req, res) {
-	const db = req.db;
-	const collection = await db.collection('games');
-	const data = await collection.find({}).toArray();
-	res.json(data);
-}
-
-module.exports =  { fetchGames };
--- server/server.js
+++ server/server.js
@@ -1,17 +1,51 @@
 const express = require('express');
-const connectToDatabase = require('./db'); // Import the connection function
-const gameRoute = require('./routes/games.js');
+const {MongoClient} = require('mongodb');
+require('dotenv').config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+const MONGODB_URI = process.env.MONGODB_URI;
 
 app.use(express.json());
 
-app.use('/games', gameRoute);
+async function listDatabases(client) {
+	databasesList = await client.db().admin().listDatabases();
 
-app.listen(PORT, async () => {
-	await connectToDatabase(function (err) {
-		if (err) console.error(`server.js: Something went wrong: ${err}`);
-	});
+	console.log("Databases:");
+	databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
+async function getGames(client) {
+	gamesList = await client.db("baseball-website").collection("games").find(
+		{
+			year: 2023
+		}
+	);
+	const results = await gamesList.toArray();
+	if (results.length > 0 ) {
+		results.forEach((result, i) => {
+			console.log(`${i+1}. homeTeam: ${result.homeTeam}`);
+		})
+	}
+	else {
+		console.log(`no listings found`);
+	}
+}
+
+async function main() {
+	const client = new MongoClient(MONGODB_URI);
+	try {
+		await client.connect();
+		await getGames(client);
+	} catch (e) {
+		console.error(`Something went wrong: ${e}`);
+	} finally {
+		await client.close();
+	}
+}
+
+main();
+
+app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
 });
