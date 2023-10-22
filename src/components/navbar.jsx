2014d282e4844464b6586ae3ffcd9cc53c1184b2
jonathansharpe <jonathansharpe54@gmail.com>
2023-10-21 17:09:33 -0700

    separating more elements

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -11,16 +11,16 @@ export default function Navbar() {
 	}
 
 	return (
-		<div class='flex justify-center'>
-		<nav class='z-20 m-4 w-3/4 block gap-4 bg-zinc-50 rounded-lg drop-shadow'>
-			<div class='inline-block p-4'><a href='#'>Home Page</a></div>
-			<div class='inline-block p-4'><a href='#'>Games</a></div>
-			<div class='inline-block relative rounded-md' onClick={toggleDropdown}>
-				<button class='p-4'>Dropdown</button>
-				<div class={`${showDropdown ? `block` : 'invisible'} absolute z-10 bg-zinc-200 w-full border border-gray-400 rounded-b-md`}>
-					<a class='block p-2 hover:bg-sky-600' href='#'>park 1</a>
-					<a class='block p-2 hover:bg-sky-600' href='#'>park 1</a>
-					<a class='block p-2 hover:bg-sky-600' href='#'>park 1</a>
+		<div className='flex justify-center'>
+		<nav className='z-20 m-4 w-3/4 block gap-4 bg-zinc-50 rounded-lg drop-shadow'>
+			<div className='inline-block p-4'><a href='#'>Home Page</a></div>
+			<div className='inline-block p-4'><a href='#'>Games</a></div>
+			<div className='inline-block relative rounded-md' onClick={toggleDropdown}>
+				<button className='p-4'>Dropdown</button>
+				<div className={`${showDropdown ? `block` : 'invisible'} absolute z-10 bg-zinc-200 w-full border border-gray-400 rounded-b-md`}>
+					<a className='block p-2 hover:bg-sky-600' href='#'>park 1</a>
+					<a className='block p-2 hover:bg-sky-600' href='#'>park 1</a>
+					<a className='block p-2 hover:bg-sky-600' href='#'>park 1</a>
 				</div>
 			</div>
 		</nav>
