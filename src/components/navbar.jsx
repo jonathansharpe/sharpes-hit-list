4d085c1ae77241a4aa2adffd4df81f3e9f991ab0
jonathansharpe <jonathansharpe54@gmail.com>
2023-10-28 17:35:42 -0700

    small changes to navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,14 +1,21 @@
 'use client';
-import React from 'react';
-import { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function Navbar() {
 	const [showDropdown, setShowDropdown] = useState(false);
+	const [jsonData, setJsonData] = useState(null);
 
 	function toggleDropdown() {
 		setShowDropdown(!showDropdown);
 		// console.log("test");
 	}
+
+	useEffect(() => {
+		fetch(import.meta.env.VITE_API_BASEURL + "/api/venues/getAllVenues")
+			.then(response => response.json())
+			.then(data => setJsonData(data))
+			.catch(error => console.error('Error fetching data:', error));
+	}, []);
 
 	return (
 		<div className='flex justify-center'>
@@ -16,14 +23,22 @@ export default function Navbar() {
 			<div className='inline-block p-4'><a href='#'>Home Page</a></div>
 			<div className='inline-block p-4'><a href='#'>Games</a></div>
 			<div className='inline-block relative rounded-md' onClick={toggleDropdown}>
-				<button className='p-4'>Dropdown</button>
+				<button className='p-4'>Park Reviews</button>
 				<div className={`${showDropdown ? `block` : 'invisible'} absolute z-10 bg-zinc-200 w-full border border-gray-400 rounded-b-md`}>
-					<a className='block p-2 hover:bg-sky-600' href='#'>park 1</a>
-					<a className='block p-2 hover:bg-sky-600' href='#'>park 1</a>
-					<a className='block p-2 hover:bg-sky-600' href='#'>park 1</a>
+		{jsonData ? (
+			<div>
+			{jsonData.map((item, index) => (
+				<a className='block p-2 hover:bg-sky-600' href='#'>
+				{item.curName}
+				</a>
+			))}
+			</div>
+		) : (
+			<p>Loading data...</p>
+		)}
 				</div>
 			</div>
 		</nav>
 		</div>
-	)
+	);
 }
