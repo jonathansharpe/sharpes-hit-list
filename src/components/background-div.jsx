c2e216399414e94a9fee776b88c79e7e23d0e520
jonathansharpe <jonathansharpe54@gmail.com>
2023-10-28 22:16:26 -0700

    searchable dropdown list yay

--- src/components/background-div.jsx
+++ src/components/background-div.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 export default function BackgroundDiv({children}) {
 	return (
-		<div className='bg-zinc-200 font-dmSans'>
+		<div className='bg-zinc-200 text-slate-950 font-dmSans'>
 		{children}
 		</div>
 	)
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,6 +4,7 @@ import React, { useState, useEffect } from 'react';
 export default function Navbar() {
 	const [showDropdown, setShowDropdown] = useState(false);
 	const [jsonData, setJsonData] = useState(null);
+	const [query, setQuery] = useState("");
 
 	function toggleDropdown() {
 		setShowDropdown(!showDropdown);
@@ -17,21 +18,30 @@ export default function Navbar() {
 			.catch(error => console.error('Error fetching data:', error));
 	}, []);
 
+
 	return (
 		<div className='flex justify-center'>
 		<nav className='z-20 m-4 w-3/4 block gap-4 bg-zinc-50 rounded-lg drop-shadow'>
 			<div className='inline-block p-4'><a href='#'>Home Page</a></div>
 			<div className='inline-block p-4'><a href='#'>Games</a></div>
 			<div className='inline-block relative rounded-md' onClick={toggleDropdown}>
-				<button className='p-4'>Park Reviews</button>
-				<div className={`${showDropdown ? `block` : 'invisible'} absolute z-10 bg-zinc-200 w-full border border-gray-400 rounded-b-md`}>
+				<input type='text' className='p-4 w-48 text-slate-950 bg-zinc-50' placeholder='Park Reviews' onChange={event => setQuery(event.target.value)}></input>
+				<div className={`${showDropdown ? `block` : 'invisible'} absolute z-10 bg-zinc-200 w-auto max-h-48 border border-gray-400 rounded-b-md overflow-scroll`}>
 		{jsonData ? (
 			<div>
-			{jsonData.map((item, index) => (
-				<a className='block p-2 hover:bg-sky-600' href='#'>
-				{item.curName}
-				</a>
-			))}
+			{
+				jsonData.filter(venue => {
+					if (query === "") {
+						return venue;
+					}
+					else if (venue.curName.toLowerCase().includes(query.toLowerCase())) {
+						return venue;
+					}
+				}).map((item, index) => (
+					<a className='block p-2 hover:bg-sky-600' href='#'>
+					{item.curName}
+					</a>
+				))}
 			</div>
 		) : (
 			<p>Loading data...</p>
