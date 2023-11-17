f9c4c23c8bc8ee7a85368f6b75657282a4efb8d5
jonathansharpe <jonathansharpe54@gmail.com>
2023-11-16 21:45:44 -0800

    made a ton of progress on the games page

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,6 +6,7 @@ export default function Navbar() {
 	const [jsonData, setJsonData] = useState(null);
 	const [isMlbOpen, setMlbIsOpen] = useState(false);
 	const [isStOpen, setStIsOpen] = useState(false);
+	const [loading, setLoading] = useState(true);
 	const mlbDropdownRef = useRef();
 	const stDropdownRef = useRef();
 
@@ -17,9 +18,11 @@ export default function Navbar() {
 				const response = await fetch(import.meta.env.VITE_API_BASEURL + "/api/venues/getAllVenues");
 				const data = await response.json();
 				setJsonData(data);
-				console.log(data);
+				// console.log(data);
 			} catch (error) {
 				console.error(`Error fetching data: ${error}`);
+			} finally {
+				setLoading(false);
 			}
 		};
 		fetchData();
@@ -41,27 +44,31 @@ export default function Navbar() {
 		}
 	}, [isMlbOpen, isStOpen]);
 
-	if (!jsonData) {
-		return null;
-	}
-
-	const mlbParks = jsonData.filter(park => !park.springTraining);
-	const stParks = jsonData.filter(park => park.springTraining);
+	const mlbParks = jsonData?.filter(park => !park.springTraining);
+	const stParks = jsonData?.filter(park => park.springTraining);
 
 	return (
 		<nav className='font-manrope flex font-bold sticky top-0 z-20 mb-4 w-full block bg-zinc-50 rounded-b-lg drop-shadow items-center'>
 			<a className='inline-block p-4 rounded-md hover:bg-zinc-200 transition-all font-syne text-xl border border-gray-400' href='/'>Sharpe's Hit List</a>
-			<a className='inline-block p-4 rounded-md hover:bg-zinc-200 transition-all' href='#'>Games</a>
+			<a className='inline-block p-4 rounded-md hover:bg-zinc-200 transition-all' href='/games/index.html'>Games</a>
 			<div ref={mlbDropdownRef}>
 				<button className='inline-block p-4 rounded-md hover:bg-zinc-200 transition-all' onClick={() => setMlbIsOpen(!isMlbOpen)}>MLB Parks</button>
 				<div className={`${isMlbOpen ? 'block' : 'invisible'} `}>
-					<Dropdown isOpen={isMlbOpen} setIsOpen={setMlbIsOpen} parks={mlbParks} />
+					{ loading ?
+						<div></div>
+						:
+						<Dropdown isOpen={isMlbOpen} setIsOpen={setMlbIsOpen} parks={mlbParks} />
+					}
 				</div>
 			</div>
 			<div ref={stDropdownRef}>
 				<button className='inline-block p-4 rounded-md hover:bg-zinc-200 transition-all' onClick={() => setStIsOpen(!isStOpen)}>Spring Training Parks</button>
 				<div className={`${isStOpen ? 'block' : 'invisible'} `}>
-					<Dropdown isOpen={isStOpen} setIsOpen={setStIsOpen} parks={stParks} />
+					{ loading ?
+						<div></div>
+						:
+						<Dropdown isOpen={isStOpen} setIsOpen={setStIsOpen} parks={stParks} />
+					}
 				</div>
 			</div>
 		</nav>
---
+++ src/games/app.jsx
@@ -0,0 +1,159 @@
+import React, { useState, useEffect } from 'react';
+import Essentials from './../components/essentials.jsx';
+import Paragraph from './../components/paragraph.jsx';
+import SectionHeader from './../components/park-review-header.jsx';
+import MainTextDiv from './../components/main-text-div.jsx';
+
+export default function App(){
+	const [gamesData, setGamesData] = useState(null);
+	const [teamsData, setTeamsData] = useState(null);
+	const [venueData, setVenueData] = useState(null);
+	const [expandedDivs, setExpandedDivs] = useState(false);
+
+	useEffect(() => {
+		const fetchGameData = async () => {
+			try {
+				const response = await fetch(import.meta.env.VITE_API_BASEURL + "/api/games/getAllGames");
+				const data = await response.json();
+				setGamesData(data);
+				// console.log(data);
+			} catch (error) {
+				console.error(`Error fetching data: ${error}`);
+			}
+		};
+		const fetchTeamData = async () => {
+			try {
+				const response = await fetch(import.meta.env.VITE_API_BASEURL + "/api/teams/getAllTeams");
+				const data = await response.json();
+				setTeamsData(data);
+				// console.log(data);
+			} catch (error) {
+				console.error(`Error fetching data: ${error}`);
+			}
+		}
+		const fetchVenueData = async () => {
+			try {
+				const response = await fetch(import.meta.env.VITE_API_BASEURL + "/api/venues/getAllVenues");
+				const data = await response.json();
+				setVenueData(data);
+				// console.log(data);
+			} catch (error) {
+				console.error(`Error fetching data: ${error}`);
+			}
+		}
+		const handleEscapeKey = (event) => {
+			if (event.key === 'Escape') {
+				setExpandedDivs({});
+			}
+		};
+		document.addEventListener('keydown', handleEscapeKey);
+		
+		fetchGameData();
+		fetchTeamData();
+		fetchVenueData();
+		return () => {
+			document.removeEventListener('keydown', handleEscapeKey);
+		};
+	}, []);
+
+	const sortGamesByDate = (games) => {
+		return games.sort((a, b) => {
+			const dateA = new Date (a.year, a.month - 1, a.day);
+			const dateB = new Date (b.year, b.month - 1, b.day);
+
+			return dateA - dateB;
+		});
+	};
+
+	function makeUrl(game) {
+		const formattedMonth = game.month.toString().padStart(2, '0');
+		const formattedDay = game.day.toString().padStart(2, '0');
+		return `https://www.baseball-reference.com/boxes/${game.homeTeam}/${game.homeTeam}${game.year}${formattedMonth}${formattedDay}0.shtml`;
+	}
+
+	if (!gamesData || !teamsData || !venueData) {
+		return null;
+	}
+	
+	const handleExpansion = (index) => {
+		setExpandedDivs((prevExpandedDivs) => ({
+			...prevExpandedDivs,
+			[index]: !prevExpandedDivs[index],
+		}));
+	};
+
+	// console.log(gamesData);
+	const numColumns = 3;
+	const numRows = Math.ceil(gamesData.length / numColumns);
+	const sortedGames = sortGamesByDate(gamesData);
+	// console.log(sortedGames);
+
+	return (
+		<Essentials>
+		<div className={`hidden fixed left-0 top-0 z-40 w-full h-full inset-0 bg-gray-900 bg-opacity-60`}></div>
+		<div className="flex w-10/12 font-rubik">
+		<div className="flex-none w-1/5 mb-4 mr-4 ml-4 p-2 bg-zinc-50 rounded rounded-lg">
+			this is where the filters will go
+		</div>
+		<div className={`flex-none grid grid-cols-${numColumns} w-4/5 mb-4 mr-4 ml-4 p-2 rounded rounded-lg border`}>
+			this is where all the games will go
+		{sortedGames.map((curGame, index) => {
+			if (curGame) {
+				const curDate = new Date(curGame.year, curGame.month - 1, curGame.day);
+				const options = { weekday: 'short', month: 'short', day: 'numeric', year: 'numeric' };
+				const formattedDate = new Intl.DateTimeFormat('en-US', options).format(curDate);
+				return (
+					<div key={index} className={`${expandedDivs[index] ? 'fixed w-9/12 left-1/2 z-50 -translate-x-1/2 transition-opacity' : 'items-center flex-1'} text-sm p-2 bg-zinc-50 m-2 rounded-md ease-in-out`}>
+					<div className="relative h-16 overflow-hidden rounded-md w-full bg-center bg-cover">
+					<img src={`../images/${curGame.venue}.jpg`} alt={curGame.venue} className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'/>
+					</div>
+					{/* date */}
+					<div className="p-1 font-bold text-lg">
+					{formattedDate}
+					</div>
+					{/* road team followed by home team */}
+					<div className="grid grid-cols-[5fr,1fr] items-center">
+					<div className="p-1">
+					{teamsData.map((team) => {
+						if (team.abbreviation === curGame.roadTeam) {
+							return (
+								team.fullName
+							)
+						}
+					})}
+					</div>
+					<div className="p-1 text-right">{curGame.roadTeamRuns}</div>
+					<div className="p-1">
+					{teamsData.map((team) => {
+						if (team.abbreviation === curGame.homeTeam) {
+							return (
+								team.fullName
+							)
+						}
+					})}
+					</div>
+					<div className="p-1 text-right">{curGame.homeTeamRuns}</div>
+					</div>
+					{ curGame.springTraining ?
+						<button onClick={() => handleExpansion(index)} type="button" className="p-1 text-center w-full rounded rounded-md bg-indigo-500 hover:bg-indigo-300 transition-all text-white">
+						Game Log
+						</button>
+						:
+						<div className="grid grid-cols-2 rounded rounded-md bg-indigo-500">
+						<button onClick={() => handleExpansion(index)} type="button" className="p-1 rounded-l-md text-center hover:bg-indigo-300 transition-all text-white">
+						Game Log
+						</button>
+						<a className="p-1 text-center hover:bg-indigo-300 rounded-r-md transition-all text-white" href={makeUrl(curGame)} target="_blank">
+						Boxscore
+						</a>
+						</div>
+					}
+					</div>
+				);
+			}
+		})}
+		</div>
+		</div>
+		</Essentials>
+	)
+}
