2fd5bed95ff6bd9eeae05e26898336c6ae15ae02
jonathansharpe <jonathansharpe54@gmail.com>
2023-12-09 17:32:07 -0800

    testing something

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// testing something
+
 const root = resolve(__dirname, 'src');
 const outDir = resolve(__dirname, 'dist');
 
